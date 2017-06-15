5ba41be1b51688f838389ff2246398e19a203472
vymiyai <vymiyai@gmail.com>
2017-06-15 13:30:19 +0200

    Added junit test for BasicCommandHandler.

--- src/main/java/com/memories_of_war/bot/gow_bot/BasicCommandHandler.java
+++ src/main/java/com/memories_of_war/bot/gow_bot/BasicCommandHandler.java
@@ -1,28 +1,61 @@
 package com.memories_of_war.bot.gow_bot;
 
+import java.util.HashMap;
+
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.RequestBuffer;
 
-public class BasicCommandHandler 
-{	
-    @EventSubscriber
-    public void onMessageReceived(MessageReceivedEvent event) 
-    {
-    	// send a message back.
-    	RequestBuffer.request(() -> 
-    	{
-            try
-            {
-                event.getChannel().sendMessage( ":UR:" );
-            } 
-            catch (DiscordException e)
-            {
-                System.err.println("Message could not be sent with error: ");
-                e.printStackTrace();
-            }
-    	});
-    }
-	
+public class BasicCommandHandler {
+
+	public String[] tokenize(String messageString) {
+		
+		return messageString.split( " " );
+	}
+
+	// Helper functions to make certain aspects of the bot easier to use.
+	private void sendMessage(IChannel channel, String message) {
+
+		// This might look weird but it'll be explained in another page.
+		RequestBuffer.request(() -> {
+			try {
+				channel.sendMessage(message);
+			} catch (DiscordException e) {
+				System.err.println("Message could not be sent with error: ");
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@EventSubscriber
+	public void onMessageReceived(MessageReceivedEvent event) {
+		/*
+		 * Type !kenjii to summon Kenjii. Type !vv for awesomeness. Type
+		 * !rip @someone for additional VietnamVetness. Type !flip to toss a
+		 * coin. Type !roll xdy with x and y being positive integer numbers to
+		 * roll x dices of y faces (e.g. !roll 1d6). Type !help to show this
+		 * tooltip.
+		 */
+
+		// get command dictionary.
+		// HashMap<String, >
+
+		// match command with dictionary key
+		// if exists, do stuff
+		// else, do nothing.
+		// for each command in command
+		
+		String messageString = event.getMessage().getContent();
+		String[] tokenizedMessageString = this.tokenize(messageString);
+/*
+		if(tokenizedMessageString.length > 0){
+		
+			// send a message back.
+		}
+	*/	
+		// not entirely sure whether an empty string can be sent 
+	}
+
 }
---
+++ src/test/java/com/memories_of_war/bot/tests/BasicCommandHandlerTest.java
@@ -0,0 +1,20 @@
+package com.memories_of_war.bot.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.memories_of_war.bot.gow_bot.BasicCommandHandler;
+
+public class BasicCommandHandlerTest {
+	
+	@Test
+	public void testTokenize()
+	{
+		BasicCommandHandler bch = new BasicCommandHandler();
+		
+		assertEquals(1, bch.tokenize("").length);
+		assertEquals(2, bch.tokenize("!vv bitch").length);
+		assertEquals(1, bch.tokenize("!resources").length);
+	}
+}
