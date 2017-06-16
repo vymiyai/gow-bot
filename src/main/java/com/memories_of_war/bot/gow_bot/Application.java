cf5d63118f2e160ca1400bd956ef03b29d30c16f
vymiyai <vymiyai@gmail.com>
2017-06-16 13:08:24 +0200

    Added ComponentScan.

--- src/main/java/com/memories_of_war/bot/gow_bot/Application.java
+++ src/main/java/com/memories_of_war/bot/gow_bot/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 
 @SpringBootApplication
+@ComponentScan("com.memories_of_war.bot.commands")
 @RestController
 public class Application {
 
