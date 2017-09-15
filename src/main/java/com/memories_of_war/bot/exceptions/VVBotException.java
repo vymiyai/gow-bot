b667557228ddb5dfe880d7fe15179139128b7fcd
Victor Yoshiaki Miyai <victor.miyai@deposit-solutions.com>
2017-09-15 21:42:23 +0200

    Added the quote command.

--- src/main/java/com/memories_of_war/bot/exceptions/VVBotException.java
+++ src/main/java/com/memories_of_war/bot/exceptions/VVBotException.java
@@ -1,4 +1,12 @@
 package com.memories_of_war.bot.exceptions;
 
-public class VVBotException extends Exception{
+public class VVBotException extends Exception {
+
+    public VVBotException() {
+        super();
+    }
+
+    public VVBotException(String errorMessage) {
+        super(errorMessage);
+    }
 }
